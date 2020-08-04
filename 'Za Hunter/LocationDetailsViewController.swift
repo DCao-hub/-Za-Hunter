a78337ce5f823ab4ea0f19511a441ef79700c17f
diana <dianacao777@gmail.com>
2020-08-03 23:32:51 -0400

    finished setting up location details VC (when you click on blue i button the info of the pizza place is displayed)

--- 'Za Hunter/LocationDetailsViewController.swift
+++ 'Za Hunter/LocationDetailsViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 import MapKit
 
 class LocationDetailsViewController: UIViewController {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
     
     var selectedMapItem = MKMapItem() //pass to this VC a selected map item
     
@@ -17,5 +20,15 @@ class LocationDetailsViewController: UIViewController {
         super.viewDidLoad()
         print(selectedMapItem)
     }
-    
+    override func viewWillAppear(_ animated: Bool) {
+        nameLabel.text = selectedMapItem.placemark.name
+        var address = selectedMapItem.placemark.subThoroughfare! + " "
+        address += selectedMapItem.placemark.thoroughfare! + "\n"
+        address += selectedMapItem.placemark.locality! + ", "
+        address += selectedMapItem.placemark.administrativeArea! + " "
+        address += selectedMapItem.placemark.postalCode!
+        addressLabel.text = address
+        phoneLabel.text = selectedMapItem.phoneNumber
+        
+    }
 }
